c81a322a655379bcb2076a82d18ceab505637c36
Michael Treser <armikael2@gmail.com>
2018-11-19 18:06:06 +0200

    Add project files.

---
+++ CS-Sandbox/GradesBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Sandbox
+{
+    class GradesBook
+    {
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades = new List<float>();
+    }
+}
---
+++ CS-Sandbox/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CS_Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name + "!");
+
+
+            Console.WriteLine("How old are you?");
+            var age = int.Parse(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("You can enter.");
+            }
+            else
+            {
+                Console.WriteLine("You are not allowed to enter");
+            }
+
+            GradesBook book = new GradesBook();
+            book.AddGrade(99);
+            book.AddGrade(77);
+
+            // GradeStatistics stats = book.getAverage();
+        }
+    }
+}
